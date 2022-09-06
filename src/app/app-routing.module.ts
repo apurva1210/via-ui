58ebe77751f29b70e14a38d39d62e18e85346240
apurvathorat <apurva.thorat@wrxflo.com>
2022-09-06 22:59:04 +0530

    added base, header, category-navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HeaderComponent} from "./layouts/header/header.component";
+import {HomeComponent} from "./pages/home/home.component";
+import {SingleCategoryComponent} from "./pages/single-category/single-category.component";
+import {SinglePostComponent} from "./pages/single-post/single-post.component";
+import {AboutUsComponent} from "./pages/about-us/about-us.component";
+import {TermsAndConditionsComponent} from "./pages/terms-and-conditions/terms-and-conditions.component";
+import {ContactUsComponent} from "./pages/contact-us/contact-us.component";
 
 const routes: Routes = [
-  {path:'', component: HeaderComponent}
+  {path:'', component: HomeComponent},
+  {path:'category', component: SingleCategoryComponent},
+  {path:'post', component: SinglePostComponent},
+  {path:'about', component: AboutUsComponent},
+  {path:'terms-conditions', component: TermsAndConditionsComponent},
+  {path:'contact', component: ContactUsComponent},
 ];
 
 @NgModule({
